a5b032907f8bdd5503e32ea8c51400f74e77f1ea
ydmins <dr.ydmins@gmail.com>
2025-01-26 22:09:56 +0900

    feature : screen_nori renders based on the received date from the screen_index

--- lib/screens/screen_index.dart
+++ lib/screens/screen_index.dart
@@ -12,7 +12,9 @@ class _ScreenIndexState extends State<ScreenIndex> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Index'),
+        title: Text(
+          'TULOVNORI',
+        ),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -20,9 +22,13 @@ class _ScreenIndexState extends State<ScreenIndex> {
         children: [
           Center(
             child: ElevatedButton(
-              child: Text('NORI'),
+              child: Text('윷놀이 도구'),
               onPressed: () async {
-                await Navigator.pushNamed(context, '/nori');
+                await Navigator.pushNamed(context, '/nori', arguments: {
+                  "nori_name": "윷놀이 도구",
+                  "nori_description":
+                      "윷놀이에 필요한 윷을 던져보세요. 전통적인 방식대로 결과에 따라 말판을 이동시키고 즐기세요.",
+                });
               },
             ),
           )
--- lib/screens/screen_nori.dart
+++ lib/screens/screen_nori.dart
@@ -10,10 +10,23 @@ class ScreenNori extends StatefulWidget {
 class _NoriState extends State<ScreenNori> {
   @override
   Widget build(BuildContext context) {
+    Map<String, Object> args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, Object>;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('NORI'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('${args["nori_name"]}'),
+        ),
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text('${args["nori_name"]}'),
+            ),
+            Text('${args["nori_description"]}'),
+            ElevatedButton(child: Text('PLAY'), onPressed: () async {})
+          ],
+        ));
   }
 }
