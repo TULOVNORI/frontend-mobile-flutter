70e5c41b3daf57c1cf117ffbc97fd5cc804ca03c
ydmins <dr.ydmins@gmail.com>
2025-01-30 15:57:35 +0900

    feature : implement card draw feature

--- lib/plays/deck/play_screen_deck.dart
+++ lib/plays/deck/play_screen_deck.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:tulovnori/plays/deck/one_card_drawer.dart';
 
 class PlayScreenDeck extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF152b39),
-        leading: IconButton(
-          icon: const Icon(Icons.chevron_left),
-          color: Colors.white,
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
+          backgroundColor: Color(0xFF1f6b01),
+          leading: Container(),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.cancel),
+              iconSize: 35,
+              color: Colors.white,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ]),
       body: Container(
-        child: Center(
-          child: Text(
-            'Deck Screen',
+        color: Color(0xFF1f6b01),
+        child: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('images/deck/background.png'),
+          )),
+          child: Center(
+            child: Column(children: [
+              OneCardDrawer(),
+            ]),
           ),
         ),
       ),
